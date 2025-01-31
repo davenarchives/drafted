c9260eae6f24c073df17b9356676901cfe29a4dd
davenarchives <daventheabyss@gmail.com>
2025-01-31 22:21:49 +0800

    submit and browse notice

--- index.php
+++ index.php
@@ -33,9 +33,11 @@ $page = $_GET['page'] ?? 'home';
         <?php
         switch ($page) {
             case 'browse':
+                include 'browsenote.php';
                 include 'browse.php';
                 break;
             case 'submit':
+                include 'submitnote.php';
                 include 'submit.php';
                 break;
             default:
---
+++ submitnote.php
@@ -0,0 +1,10 @@
+<div class="notice-container">
+    <div class="notice">
+        <p>
+            <strong>
+                <i class='bx bx-info-circle'></i> Important Note:
+            </strong>
+            Message deletion is not available. Please ensure your message is carefully thought out before submitting, and refrain from using any defamatory, offensive, or inappropriate language. Thank you for your cooperation!
+        </p>
+    </div>
+</div>
